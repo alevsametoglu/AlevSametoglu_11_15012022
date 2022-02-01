c0a58ce86bd263ef2618d39016c1e80e9b288222
alev sametoglu <alevsametoglu.fr@gmail.com>
2022-02-01 11:08:13 +0100

    import components

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,5 +2,20 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Banner from "./Banner/Banner";
 import Card from "./Card/Card";
+import Collapsible from "./Collapsible/Collapsible";
+import Slider from "./Slider/Slider";
+import Tag from "./Tag/Tag";
+import Host from "./Host/Host";
+import Profile from "./Profile/Profile";
 
-export { Header, Footer, Banner, Card };
+export {
+  Header,
+  Footer,
+  Banner,
+  Card,
+  Collapsible,
+  Slider,
+  Tag,
+  Host,
+  Profile,
+};
